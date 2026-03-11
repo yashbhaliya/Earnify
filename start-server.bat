a652e63db1be3d45576e17706c0a27a7f5323b89
yash <bhaliyayash595@gmail.com>
2026-03-11 14:02:26 +0530

    first commit

--- start-server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting Earnify Server...
-echo.
-echo Server will run on: http://localhost:5000
-echo Admin Panel: http://localhost:5000/admin/statistics.html
-echo.
-node server.js
